d3a810373119ccd4ef5641baa4da9783639c1bf4
pyrrhusm <stepmac163pyr@gmail.com>
2026-04-13 15:19:54 -0400

    added the raw text and random story string function and changed the name of the javascript file

--- assignment4/part1/javascript/main.js
+++ assignment4/part1/javascript/main.js
@@ -18,22 +18,18 @@ function randomValueFromArray(array) {
 
 // Raw text strings
 
-// Willy the Goblin
-// Big Daddy
-// Father Christmas
-
-// the soup kitchen
-// Disneyland
-// the White House
-
-// spontaneously combusted
-// melted into a puddle on the sidewalk
-// turned into a slug and slithered away
+const characters = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+const places = ["the soup kitchen", "Disneyland", "the White House"];
+const events = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and slithered away"];
 
 // Partial return random string function
 
 function returnRandomStoryString() {
-  // It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
+  const randomCharacter = randomValueFromArray(characters)
+  const randomPlace = randomValueFromArray(places)
+  const randomEvent = randomValueFromArray(events)
+
+  let storyText = "It was 94 Fahrenheit outside, so ${randomCharacter} went for a walk. When they got to ${randomPlace}, they stared in horror for a few moments, then ${randomEvent}. Bob saw the whole thing, but was not surprised — :customCharacter: weighs 300 pounds, and it was a hot day.";
 
   return storyText;
 }
@@ -43,6 +39,7 @@ function returnRandomStoryString() {
 generateBtn.addEventListener("click", generateStory);
 
 function generateStory() {
+  let newStory = storyText;
   if (customName.value !== "") {
     const name = customName.value;
   }
