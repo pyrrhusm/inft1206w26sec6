5fed9fd0863053107b6bf2a165239b7fc7def3c3
pyrrhusm <stepmac163pyr@gmail.com>
2026-04-13 17:46:42 -0400

    Finished implementing the ball loop and the ball collision detection

--- assignment4/part3/js/main.js
+++ assignment4/part3/js/main.js
@@ -46,12 +46,82 @@ class Ball {
     ctx.fill();
   }
 
-  
+  // Update the balls location to move it
+  update() {
+    if (this.x + this.size >= width) {
+      this.velX = -this.velX;
+    }
+
+    if (this.x - this.size <= 0) {
+      this.velX = -this.velX;
+    }
+
+    if (this.y + this.size >= height) {
+      this.velY = -this.velY;
+    }
+
+    if (this.y - this.size <= 0) {
+      this.velY = -this.velY;
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+
+  // Adding collision detection
+  collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
 }
 
-const testBall = new Ball(50, 100, 4, 4, "blue", 10);
 
-testBall.x;
-testBall.size;
-testBall.color;
-testBall.draw();
+// Animating the balls
+
+const balls = [];
+
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size,
+  );
+
+  balls.push(ball);
+}
+
+
+// Creating the loop function
+
+function loop() {
+  ctx.fillStyle = "rgb(0 0 0 / 25%)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+    ball.collisionDetect();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+
+// Starting the loop
+
+loop();
