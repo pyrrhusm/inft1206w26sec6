b0f6aff701d1c96c7ea241e726767be449979d51
pyrrhusm <stepmac163pyr@gmail.com>
2026-04-13 16:15:55 -0400

    Added the comments to all 3 files for assignment 4 part 2 as well as the link for css and script for js files

--- assignment4/part2/js/main.js
+++ assignment4/part2/js/main.js
@@ -1,3 +1,10 @@
+/**
+ * @fileoverview Creating the code for an image gallery
+ * @author Pyrrhus MacKinnon
+ * @date 2026-04-13
+ * @version 1.0.0
+ */
+
 const displayedImage = document.querySelector(".displayed-img");
 const thumbBar = document.querySelector(".thumb-bar");
 
