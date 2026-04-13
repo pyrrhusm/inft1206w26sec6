8cc40a831439ab62567f90fd15c38daf26fa97d7
pyrrhusm <stepmac163pyr@gmail.com>
2026-04-13 17:34:34 -0400

    Added the Ball class and tested that it works

--- assignment4/part3/js/main.js
+++ assignment4/part3/js/main.js
@@ -17,3 +17,34 @@ function random(min, max) {
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
+
+// Adding the class for the ball
+
+class Ball {
+  // Constructor
+  constructor(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+  }
+
+  // Drawing the ball
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  
+}
+
+const testBall = new Ball(50, 100, 4, 4, "blue", 10);
+
+testBall.x;
+testBall.size;
+testBall.color;
+testBall.draw();
