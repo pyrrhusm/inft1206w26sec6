1ca638c0199a30acc0c3b37b73988465eb7a6928
pyrrhusm <stepmac163pyr@gmail.com>
2026-04-13 16:43:05 -0400

    added images and wrote the code up until the function addition of updateDisplayedImages

---
+++ assignment4/part2/js/script.js
@@ -0,0 +1,29 @@
+/**
+ * @fileoverview Creating the code for an image gallery
+ * @author Pyrrhus MacKinnon
+ * @date 2026-04-13
+ * @version 1.0.0
+ */
+
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
+
+const images = [
+    {filename: "pic1.jpg", alt: "Closeup of a human eye"},
+    {filename: "pic2.jpg", alt: "Rock that looks like a wave"},
+    {filename: "pic3.jpg", alt: "Purple and white pansies"},
+    {filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb"},
+    {filename: "pic5.jpg", alt: "Large moth on a leaf"}
+];
+
+const baseURL = "./images/";
+
+// Looping through the images
+for (const image of images) {
+    const newImage = document.createElement("img");
+    newImage.src = `${baseURL}${image.filename}`;
+    newImage.alt = image.alt;
+    newImage.tabIndex = "0"
+}
